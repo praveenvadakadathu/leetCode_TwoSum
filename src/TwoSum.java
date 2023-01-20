dc8d34b0feaf3dae46ba8bc10a3fe077f7a9b2f3
unknown <vadakadathupraveen@gmail.com>
2023-01-21 00:14:47 +0530

    Firsttry

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
 
 public class TwoSum {
 
 	public static void main(String[] args) {
-		System.out.println("HI there");
-
+		System.out.println(Arrays.toString(twoSum(new int[] { 2, 5, 5, 11 }, 10)));
 	}
 
+	public static int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+
+			for (int j = 1; j < nums.length; j++) {
+
+				if ((nums[i] + nums[j] == target) && i != j) {
+
+					return new int[] { i, j };
+				}
+			}
+		}
+		return new int[0];
+	}
 }
