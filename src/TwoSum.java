697dd482154602fdb07424b3d0da7ab158c06ee8
unknown <vadakadathupraveen@gmail.com>
2023-01-21 20:17:16 +0530

    most optimal approach

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,22 +1,24 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(twoSum(new int[] { 2, 5, 5, 11 }, 10)));
+		System.out.println(Arrays.toString(twoSum(new int[] { 2, 5, 6, 11 }, 11)));
 	}
 
 	public static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> comps = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
+			int comp = target - nums[i];
+			if (comps.containsKey(nums[i])) {
 
-			for (int j = 1; j < nums.length; j++) {
-
-				if ((nums[i] + nums[j] == target) && i != j) {
-
-					return new int[] { i, j };
-				}
+				return new int[] { comps.get(nums[i]), i };
 			}
+			comps.put(comp, i);
 		}
+
 		return new int[0];
 	}
 }
